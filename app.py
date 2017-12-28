2bb481d24438bb15c21ab9a00eb6bf266ead3503
Ronald Tsung <Me@Ronald-Tsungs-MacBook-Pro.local>
2017-12-28 11:35:49 -0500

    redirect

--- app.py
+++ app.py
@@ -9,13 +9,12 @@ app = Flask(__name__)
 
 # mongo = PyMongo(app)
 client = MongoClient("mongodb://mars:rams@ds243345.mlab.com:43345/heroku_m5kl8br7")
-db = heroku_m5kl8br7
+db =  client.heroku_m5kl8br7
 mars = db.mars
 
 # Home route
 @app.route("/")
 def home():
-    df = pd.read_csv('df.csv')
     # mars = mongo.db.mars.find_one()
     mars = mars.find_one()
     return render_template("index.html", mars=mars)
@@ -32,7 +31,8 @@ def send():
         upsert=True
     )
     print("about to redirect")
-    return redirect("http://localhost:5000/", code=302)
+    # return redirect("http://localhost:5000/", code=302)
+    return redirect("https://mongo-.herokuapp.com/", code=302)
 
 if __name__ == '__main__':
     app.run(debug=False)
